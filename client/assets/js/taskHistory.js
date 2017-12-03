9137c906bd305dc81d314b959af42379b80625ea
ArchITech <swtdavid@gmail.com>
2017-12-03 11:45:33 -0800

    .

--- client/assets/js/taskHistory.js
+++ client/assets/js/taskHistory.js
@@ -215,7 +215,7 @@ jQuery(document).ready(function(){
 			taskLogs = data.log.reverse();
 
 			if (taskLogs.length > 0) {
-				totalPages = Math.max(1, (taskLogs.length+1)/pageSize);
+				totalPages = Math.max(1, Math.ceil((taskLogs.length+1)/pageSize));
 				$('.history-pagination').twbsPagination({
 					totalPages: totalPages,
 					visiblePages: 7,
