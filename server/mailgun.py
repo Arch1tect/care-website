66d8f7ecca8c7c63add8401edceaff23916b57a9
ArchITech <swtdavid@gmail.com>
2017-12-16 21:18:16 -0800

    .

--- server/mailgun.py
+++ server/mailgun.py
@@ -6,7 +6,7 @@ def send_simple_message(subject, to, msg=None, html=None, files=None):
 		"https://api.mailgun.net/v3/sandboxd1b6c36cfcee4a788c1d9794208e7583.mailgun.org/messages",
 		auth=("api", "key-c5e95173a1f60ab411eff18031723799"),
 		files=files,
-		data={"from": "Watchmen <watchmen@donotreply.com>",
+		data={"from": "Web Watchman <watchman@donotreply.com>",
 			  "to": ["Dear", to],
 			  "subject": subject,
 			  "html": html,
@@ -29,7 +29,7 @@ def send_welcome_email(email, tmp_password):
 
 def notify_change(subject, url, img_path, img_name):
 # http://mailgun-documentation.readthedocs.io/en/latest/user_manual.html#sending-via-api
-	html = "<html>Content at <a href='{0}'>{1}</a> has changed: <img src='cid:{2}' style='max-width:400px;'></html>".format(url, url, img_name)
+	html = "<html>Content at <a href='{0}'>{1}</a> has changed: <img src='cid:{2}' style='max-width:400px;'><br/><br/><br/>Manage your tasks <a href='webwatchman.com/task.html'>here</a>.</html>".format(url, url, img_name)
 	files = [("inline", open(img_path))]
 
 	return send_simple_message(subject, 'swtdavid@gmail.com', html=html, files=files)
