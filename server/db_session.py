09f284f4c82231900bf0b7fbdce384defee01a36
ArchITech <swtdavid@gmail.com>
2018-01-05 01:45:58 -0800

    .

--- server/db_session.py
+++ server/db_session.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 
 import setup
-from cfg.credentials import db_user, db_password
+from cfg.secret import db_user, db_password
 
 db_url = 'chat-anywhere-mysql.cjwz9xnh80ai.us-west-1.rds.amazonaws.com'
 connection_str = 'mysql://{}:{}@{}/care?charset=utf8'.format(db_user, db_password, db_url)
